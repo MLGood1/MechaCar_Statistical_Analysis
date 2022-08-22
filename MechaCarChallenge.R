0cc11037ee44b82c4826dc5d3902c371910f1d86
MLGood1 <104471775+MLGood1@users.noreply.github.com>
2022-08-21 19:56:00 -0500

    Adding T.tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,11 +5,23 @@ head(mecha_mpg)
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mecha_mpg) #create multiple linear regression model
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,mecha_mpg)) #summarize multiple linear regression model
 
+#Suspension Coil
+
 suspen_coil <- read.csv('Suspension_coil.csv', check.names = F, stringsAsFactors = F)#read in dataset
 head(suspen_coil)
 
 total_summary <- suspen_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))# display summary data of PSI
 total_summary
 
+
 lot_summary <- suspen_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI),Variance = var(PSI),SD = sd(PSI), .groups = 'keep') #create summary table
 lot_summary
+
+# T-tests on suspension coils
+t.test((suspen_coil$PSI),mu = 1500) #all lots
+
+# Each lot
+
+t.test(subset(suspen_coil,Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
+t.test(subset(suspen_coil,Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
+t.test(subset(suspen_coil,Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
